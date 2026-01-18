eebe470ca69c6919880c6a5a26ff02dec1e3d2f2
Trimpsuz <me@trimpsuz.dev>
2026-01-18 16:44:59 +0200

    fix: overflow

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,8 @@ import { QRGenerator } from '@/components/QRGenerator';
 
 export default function Home() {
   return (
-    <div className="min-h-screen bg-background">
-      <div className="container mx-auto px-4 py-16 md:py-24">
+    <div className="min-h-screen bg-background flex items-center content-center">
+      <div className="container mx-auto px-4 py-16">
         <div className="text-center mb-12 md:mb-16">
           <h1 className="text-4xl md:text-5xl lg:text-6xl font-bold text-foreground tracking-tight mb-4 text-balance">QR code generator</h1>
         </div>
